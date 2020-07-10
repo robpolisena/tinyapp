1187b0199e5e89b22a6b1f90c4aea898ec383b12
Rob <robpolisena@gmail.com>
2020-07-10 00:05:58 +0000

    moved findUserByEmail function as help function

---
+++ helpers.js
@@ -0,0 +1,18 @@
+//const { users } = require('./express_server');
+
+const findUserByEmail = (email, database) => {
+  // loop through the users in the db
+  for (let userId in database) {
+    // if email match the email from the user from the db, return the user
+    if (database[userId].email === email) {
+      // return the full user object
+      return database[userId];
+    }
+  }
+  return false;
+};
+
+//console.log(findUserByEmail("user@example.com", users));
+
+
+module.exports = findUserByEmail;
