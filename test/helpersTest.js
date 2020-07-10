363b0eeab79773cd32ec68c800beee71014d41bf
Rob <robpolisena@gmail.com>
2020-07-10 00:07:59 +0000

    testing helper function with mocha chai

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -7,13 +7,13 @@ const findUserByEmail = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
@@ -22,7 +22,7 @@ describe('findUserByEmail', function() {
   it('should return a user with valid email', function() {
     console.log(findUserByEmail);
     
-    const user = findUserByEmail("user@example.com", testUsers)
+    const user = findUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
     // Write your assert statement here
     assert.equal(user.id, expectedOutput);
@@ -31,7 +31,7 @@ describe('findUserByEmail', function() {
 
 describe('findUserByEmail', function() {
   it('should return undefined if users email is not in databasel', function() {
-    const user = findUserByEmail("user33@example.com", testUsers)
+    const user = findUserByEmail("user33@example.com", testUsers);
     const expectedOutput = undefined;
     // Write your assert statement here
     assert.equal(user.id, expectedOutput);
